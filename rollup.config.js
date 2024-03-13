afb22221199afaac9efb229dbbf0f891fcaedb13
Chris Frewin <frewin.christopher@gmail.com>
2024-03-13 11:14:17 +0100

    oops, forgot to build a type declaration

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,27 @@
-import typescript from 'rollup-plugin-typescript2';
+import typescript from "rollup-plugin-typescript2";
+import dts from 'rollup-plugin-dts';
 
-export default {
-  input: 'src/index.ts',
-  output: {
-    file: 'dist/index.js',
-    format: 'cjs'
+export default [
+  // standard package
+  {
+    input: "src/index.ts",
+    output: {
+      file: "dist/index.js",
+      format: "cjs",
+    },
+    plugins: [typescript()],
   },
-  plugins: [
-    typescript()
-  ]
-};
+  // type declarations
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "dist/index.d.ts", 
+        format: "es",
+      },
+    ],
+    plugins: [
+      dts(),
+    ],
+  },
+];
