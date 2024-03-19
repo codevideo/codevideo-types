ca2542aac0b6e15fd9db9e040fec9b713a58f698
Chris Frewin <frewin.christopher@gmail.com>
2024-03-19 10:07:53 +0100

    oops, try this way to support both browser and node environments...

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,37 @@
 import typescript from "rollup-plugin-typescript2";
-import dts from 'rollup-plugin-dts';
+import dts from "rollup-plugin-dts";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
 
 export default [
   // standard ES package - used in browser environments
   {
     input: "src/index.ts",
-    output: {
-      file: "dist/index.js",
-      format: "es",
-    },
-    plugins: [typescript()],
-  },
-  // commonjs bundle - used in node environments
-  {
-    input: "src/index.ts",
-    output: {
-      file: "dist/index.cjs",
-      format: "cjs",
-    },
-    plugins: [typescript()],
+    output: [
+      {
+        file: "dist/bundle.cjs.js", // CommonJS format for Node.js
+        format: "cjs",
+      },
+      {
+        file: "dist/bundle.esm.js", // ES Module format for browsers
+        format: "es",
+      },
+    ],
+    plugins: [
+      resolve(), // resolve node_modules
+      commonjs(), // convert CommonJS to ES modules
+      typescript(),
+    ],
   },
   // type declarations
   {
     input: "src/index.ts",
     output: [
       {
-        file: "dist/index.d.ts", 
+        file: "dist/index.d.ts",
         format: "es",
       },
     ],
-    plugins: [
-      dts(),
-    ],
+    plugins: [dts()],
   },
 ];
