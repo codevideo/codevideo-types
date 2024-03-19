5efc5cd6a47ae058cca56610023a0c8e4dd370cf
Chris Frewin <frewin.christopher@gmail.com>
2024-03-19 09:58:05 +0100

    need a cjs version for node environments...

--- rollup.config.js
+++ rollup.config.js
@@ -2,12 +2,21 @@ import typescript from "rollup-plugin-typescript2";
 import dts from 'rollup-plugin-dts';
 
 export default [
-  // standard package
+  // standard ES package - used in browser environments
   {
     input: "src/index.ts",
     output: {
       file: "dist/index.js",
       format: "es",
+    },
+    plugins: [typescript()],
+  },
+  // commonjs bundle - used in node environments
+  {
+    input: "src/index.ts",
+    output: {
+      file: "dist/index.cjs",
+      format: "cjs",
     },
     plugins: [typescript()],
   },
