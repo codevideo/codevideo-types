37539cbcc1ed4b212f49b7c1eb84c759d561a91b
Chris Frewin <chris.frewin@solve.ch>
2025-02-01 08:52:47 +0100

    woops, fix file explorer type guard

--- src/type-guards/isFileExplorerAction.ts
+++ src/type-guards/isFileExplorerAction.ts
@@ -3,19 +3,19 @@ import { FileExplorerAction } from "../types/FileExplorerAction";
 
 export const isFileExplorerAction = (action: IAction): action is FileExplorerAction => {
     return (action.name as string) in {
-        'create-file': true,
-        'open-file': true,
-        'close-file': true,
-        'rename-file': true,
-        'delete-file': true,
-        'move-file': true,
-        'copy-file': true,
-        'create-folder': true,
-        'rename-folder': true,
-        'delete-folder': true,
-        'toggle-folder': true,
-        'move-folder': true,
-        'copy-folder': true,
+        "file-explorer-create-file": true,
+        "file-explorer-open-file": true,
+        "file-explorer-close-file": true,
+        "file-explorer-rename-file": true,
+        "file-explorer-delete-file": true,
+        "file-explorer-move-file": true,
+        "file-explorer-copy-file": true,
+        "file-explorer-create-folder": true,
+        "file-explorer-rename-folder": true,
+        "file-explorer-delete-folder": true,
+        "file-explorer-toggle-folder": true,
+        "file-explorer-move-folder": true,
+        "file-explorer-copy-folder": true,
     };
 }
 
