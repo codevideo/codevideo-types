5da78c07022553c8dcf5fb40bb91a26d4a35eb2a
Chris Frewin <chris.frewin@solve.ch>
2025-03-03 11:24:05 +0100

    woops, add virtual-ide to log source types

--- src/types/logs/VirtualLayerLogSource.ts
+++ src/types/logs/VirtualLayerLogSource.ts
@@ -1,1 +1,1 @@
-export type VirtualLayerLogSource = 'virtual-file-explorer' | 'virtual-editor' | 'virtual-terminal' | 'virtual-mouse' | 'virtual-author' | 'virtual-external';
+export type VirtualLayerLogSource = 'virtual-ide' | 'virtual-file-explorer' | 'virtual-editor' | 'virtual-terminal' | 'virtual-mouse' | 'virtual-author' | 'virtual-external';
