b3472836a741846ccea523d2d9ecd3c48566ec48
Chris Frewin <chris.frewin@solve.ch>
2025-03-19 21:53:29 +0100

    add environment to manifest

--- src/interfaces/video/ICodeVideoManifest.ts
+++ src/interfaces/video/ICodeVideoManifest.ts
@@ -2,6 +2,7 @@ import { IAction } from "../IAction";
 import { IAudioItem } from "./IAudioItem";
 
 export interface ICodeVideoManifest {
+  environment: string;
   userId: string;
   uuid: string;
   actions: Array<IAction>;
