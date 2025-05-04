0aaa3d46bb1068632ad3d9cf96adac3225594dbb
Chris Frewin <chris.frewin@solve.ch>
2025-05-04 20:52:08 +0200

    update manifest model

--- src/interfaces/video/ICodeVideoManifest.ts
+++ src/interfaces/video/ICodeVideoManifest.ts
@@ -1,4 +1,5 @@
 import { IAction } from "../IAction";
+import { ILesson } from "../toplevel/ILesson";
 import { IAudioItem } from "./IAudioItem";
 
 // The main data carrier for all things done with CodeVideo video exports.
@@ -6,7 +7,9 @@ export interface ICodeVideoManifest {
   environment: string;
   userId: string;
   uuid: string;
-  actions: Array<IAction>;
+  actions?: Array<IAction>;
+  lesson?: ILesson; // note: we don't have a course property here because a course is always split into multiple lesson manifests
+  currentLessonIndex: number;
   audioItems: Array<IAudioItem>;
   error?: string;
 }
