032cb439713316292e82e0f08fee53b1e2c9b535
Chris Frewin <chris.frewin@solve.ch>
2025-06-08 11:06:51 +0200

    improve logging in isValidActions

--- src/validators/isValidActions.ts
+++ src/validators/isValidActions.ts
@@ -4,10 +4,10 @@ import { isValidAction } from "./isValidAction";
 
 export const isValidActions = (project: Project): project is Array<IAction> => {
     // ensure that project is type of array and that every element is valid action
-    return Array.isArray(project) && project.every((action) => {
+    return Array.isArray(project) && project.every((action, index) => {
         const result = isValidAction(action);
         if (!result) {
-            console.log('Invalid action:', action);
+            console.log(`Invalid action at action index ${index}: ${JSON.stringify(action)}`);
         }
         return result;
     });
